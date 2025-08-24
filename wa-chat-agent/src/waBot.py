9c11004a80e99587d94bf0b001f60f40900dde60
AveraITSolutions <55059435+AveraITSolutions@users.noreply.github.com>
2025-08-24 19:05:33 +0530

    deploy

--- wa-chat-agent/src/waBot.py
+++ wa-chat-agent/src/waBot.py
@@ -29,5 +29,6 @@ async def userInput(msg: str, history):
     return result["messages"][-1].content
 
 app = gr.ChatInterface(userInput, type="messages")
-
-app.launch(server_name=os.getenv("SERVER"))
+port = int(os.environ.get("PORT", 7860))
+app.launch(server_name="0.0.0.0", server_port=port)
+#app.launch(server_name=os.getenv("SERVER"))
